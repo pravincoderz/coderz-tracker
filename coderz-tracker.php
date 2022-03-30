5dc00f6dceb2799d0dfbc521377303296ba6122a
pravincoderz <102664249+pravincoderz@users.noreply.github.com>
2022-03-30 17:58:14 +0530

    new updated

--- coderz-tracker.php
+++ coderz-tracker.php
@@ -49,8 +49,39 @@ define( 'CODERZTRAC_PLUGIN_URL',	plugin_dir_url( CODERZTRAC_PLUGIN_FILE ) );
 require_once CODERZTRAC_PLUGIN_DIR . 'core/class-coderz-tracker.php';
 
 
-    $updater = MakeitWorkPress\WP_Updater\Boot::instance();
-    $updater->add(['type' => 'plugin', 'source' => 'https://github.com/pravincoderz/coderz-tracker.git']);
+ 
+/**
+ * Updater class for WordPress plugins hosted with Github.
+ *
+ * @since   1.1
+ * @author  WP Theme Tutorial, Curtis McHale
+ * @link    https://github.com/jkudish/WordPress-GitHub-Plugin-Updater
+ */
+function sfn_gfcoupon_github_updater(){
+
+  /* includes the update from Github code */
+  require_once( plugin_dir_path( __FILE__ ) . '/updater.php' );
+
+  define('WP_GITHUB_FORCE_UPDATE', true);
+
+  if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+      $config = array(
+          'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+          'proper_folder_name' => 'coderz-tracker', // this is the name of the folder your plugin lives in
+          'api_url' => 'https://api.github.com/repos/pravincoderz/coderz-tracker', // the github API url of your github repo
+          'raw_url' => 'https://raw.github.com/pravincoderz/coderz-tracker/master', // the github raw url of your github repo
+          'github_url' => 'https://github.com/pravincoderz/coderz-tracker', // the github url of your github repo
+          'zip_url' => 'https://github.com/pravincoderz/coderz-tracker/zipball/master', // the zip url of the github repo
+          'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+          'requires' => '1.0', // which version of WordPress does your plugin require?
+          'tested' => '1.0', // which version of WordPress is your plugin tested up to?
+          'readme' => 'readme.txt' // which file to use as the readme for the version number
+      );
+      new WPGitHubUpdater($config);
+  }
+}
+add_action( 'init', 'sfn_gfcoupon_github_updater' );
+
 
 /**
  * The main function to load the only instance
